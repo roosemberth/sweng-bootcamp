1dc998f5f865448be8b9c396cc3bffb7f4a9fc0e
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-09-25 20:47:49 +0200

    Fix linting errors detected by codeclimate
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
@@ -1,5 +1,11 @@
 package ch.epfl.sweng.bootcamp;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -7,21 +13,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
   @Rule
-  public final ActivityTestRule<MainActivity> mActivityRule =
+  public final ActivityTestRule<MainActivity> mainActivityRule =
     new ActivityTestRule<>(MainActivity.class);
 
   @Test
   public void testCanGreetUsers() {
-    onView(withId(R.id.mainName)).perform(typeText("from my unit test")).perform(closeSoftKeyboard());
+    onView(withId(R.id.mainName))
+      .perform(typeText("from my unit test"))
+      .perform(closeSoftKeyboard());
     onView(withId(R.id.mainGoButton)).perform(click());
     // on View(withId(R.id.greetingMessage)).check(matched(withText("Hello from my unit test!")));
   }
